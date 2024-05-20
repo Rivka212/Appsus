8f8210f559f49c6f07d42d188ab7f8b72e76ed59
Bashari Rivka <basrivka1@gmail.com>
2024-05-21 01:21:33 +0300

    Add styling of colors and icons

--- apps/note/views/NoteIndex.jsx
+++ apps/note/views/NoteIndex.jsx
@@ -1,4 +1,16 @@
 
 export function NoteIndex() {
-    return <div>note app</div>
+    return <section><div>note app</div>
+        <i className="fa-regular fa-trash-can" />
+        <i className="fa-regular fa-palette" />
+        <i className="fa-light fa-palette" />
+        <i className="fa-regular fa-image" />
+        <i className="fa-solid fa-thumbtack" />
+        <i className="fa-sharp fa-regular fa-lightbulb" />
+        <i className="fa-sharp fa-regular fa-bell" />
+        <i className="fa-sharp fa-regular fa-pen" />
+        <i className="fa-brands fa-youtube" />
+        <i className="fa-regular fa-pen" />
+        <i className="fa-solid fa-pen-to-square" />
+    </section>
 }
--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -4,13 +4,13 @@ export function AppHeader() {
 
     return <header className="app-header">
         <Link to="/">
-            <h3>LOGO!</h3>
+            <h3>Appsus</h3>
         </Link>
         <nav>
             <NavLink to="/">Home</NavLink>
             <NavLink to="/about">About</NavLink>
-            <NavLink to="/mail">Mail</NavLink>
-            <NavLink to="/note">Note</NavLink>
+            <i className="fa-solid fa-envelope" /><NavLink to="/mail">Mail</NavLink>
+            <i className="fa-solid fa-map-pin" /><NavLink to="/note">Note</NavLink>
         </nav>
     </header>
 }
