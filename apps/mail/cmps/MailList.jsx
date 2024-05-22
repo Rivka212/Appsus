d251a18ecf98d763ac0d4e6cfb7c5c9226b6404d
sivandin <sivandin@gmail.com>
2024-05-22 08:27:46 +0300

    Add MailIndex and MailList

--- apps/mail/cmps/MailList.jsx
+++ apps/mail/cmps/MailList.jsx
@@ -1,3 +1,8 @@
-export function MailList() {
-    return <div>Mail list</div>
+export function MailList({mails}) {
+    console.log(mails)
+    return <section className="mail-list">Mail list
+<ul>
+    {mails.map(mail=> <li key={mail.id}>{mail.subject}</li>)}
+</ul>
+    </section>
 }
--- apps/mail/views/MailIndex.jsx
+++ apps/mail/views/MailIndex.jsx
@@ -1,7 +1,19 @@
+import { MailList } from '../cmps/MailList.jsx'
+import { mailService } from '../services/mail.service.js'
+const { useState, useEffect } = React
 
 export function MailIndex() {
-    return <div>mail app
-        <p>Hello Mail</p>
-    </div>
+const [mails, setMails] = useState([])
+
+useEffect (() => {
+    debugger
+mailService.query()
+.then(mails => setMails(mails))
+},[])
+
+return <section className="mail-index">
+<h1>mails</h1>
+<MailList mails={mails}/>
+</section>
 }
 
